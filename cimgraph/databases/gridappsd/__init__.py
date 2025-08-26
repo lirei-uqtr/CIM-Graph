6adbfaeccd01cce3c74203925e2ca28fa2eb865d
lirei-uqtr <149507705+lirei-uqtr@users.noreply.github.com>
2025-08-26 09:20:32 -0400

    Update __init__.py

--- cimgraph/databases/gridappsd/__init__.py
+++ cimgraph/databases/gridappsd/__init__.py
@@ -25,18 +25,3 @@ get_topology_response(feeder_mrid: str, gapps: GridAPPSD) -> Dict:
     dict
         A dictionary containing the response from the GridAPPSD platform.
 """
-from __future__ import annotations
-
-from gridappsd import GridAPPSD
-
-from cimgraph.databases.gridappsd.gridappsd import GridappsdConnection
-
-
-def get_topology_response(feeder_mrid: str, gapps: GridAPPSD) -> dict:
-    assert feeder_mrid is not None
-
-    topic = 'goss.gridappsd.request.data.topology'
-    message = {'requestType': 'GET_SWITCH_AREAS', 'modelID': feeder_mrid, 'resultFormat': 'JSON'}
-
-    topo_response = gapps.get_response(topic=topic, message=message, timeout=30)
-    return topo_response
